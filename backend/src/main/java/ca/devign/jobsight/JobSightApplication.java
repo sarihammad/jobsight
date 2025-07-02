3dba2c16a1b8c2a531e9509cc624706915a09966
Sari Hammad <sari_hammad@hotmail.com>
2025-07-02 08:07:29 -0400

    feat: init Spring Boot backend

---
+++ backend/src/main/java/ca/devign/jobsight/JobSightApplication.java
@@ -0,0 +1,13 @@
+package ca.devign.jobsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSightApplication.class, args);
+	}
+
+}
